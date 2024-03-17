449b0654c55ef0ba3012178073dba823d8acd02e
Siddhartha127 <114979239+Siddhartha127@users.noreply.github.com>
2024-03-17 22:33:32 +0530

    second-4

--- second/src/components/Button/Button.jsx
+++ second/src/components/Button/Button.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import styles from './Button.module.css'
+import React from "react";
+import styles from "./Button.module.css";
 import { MdMessage } from "react-icons/md";
-function Button(props) {
+function Button({isOutline,icon,text,...rest}) {
   return (
-<button className={props.isOutline ? styles.outline_btn : styles.primary_btn}>
-    {props.icon}
-    {props.text}
-</button>
+    <button
+     {...rest}
+      className={isOutline ? styles.outline_btn : styles.primary_btn}
+    >
+      {icon}
+      {text}
+    </button>
   );
-};
+}
 
 export default Button;
--- second/src/components/ContactForm/ContactForm.jsx
+++ second/src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./ContactForm.module.css";
 import Button from "../Button/Button";
-import { MdMessage } from "react-icons/md";
+import { MdDisplaySettings, MdMessage } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
 
 function ContactForm() {
+  const onViaCallSubmit=()=>{
+    console.log("HEllo")
+  }
+
+
   return (
     <section className={styles.container}>
       <div className={styles.contact_form}>
         <div className={styles.top_btn}>
           <Button
+          onClick={onViaCallSubmit}
             text="VIA SUPPORT CHAT"
             icon={<MdMessage fontSize="24px" />}
           />
@@ -31,9 +37,24 @@ function ContactForm() {
                 <label htmlFor="email">Email:</label>
                 <input type="email" name="email"></input>
             </div>
+            <div className={styles.form_control}>
+                <label htmlFor="text">Text:</label>
+                <textarea name="text" rows="8"/>
+            </div>
+            <div style={{
+              display:"flex",
+              justifyContent:"end",
+            }}>
+            <Button
+            isOutline={false}
+            text="Submit Button"
+          />
+            </div>
           </form>
       </div>
-      <div className={styles.contact_image}></div>
+      <div className={styles.contact_image}>
+        <img src="\images\Service 24_7-pana 1.svg" alt="" />
+      </div>
     </section>
   );
 }
